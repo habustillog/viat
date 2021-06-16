c1d6dea6ed0ac3354821ac97e7ed34166c52848c
habustillog <79932434+habustillog@users.noreply.github.com>
2021-06-16 12:16:12 -0500

    conexion bd

--- conexion.php
+++ conexion.php
@@ -1,6 +1,14 @@
 
- 
-<?php
-$conexion = mysql_connect("localhost","root","","viats");
 
- ?>
+<?php
+
+$server = "localhost";
+$user = "root";
+$pass = "";
+$db = "viats";
+
+$conexion = new mysqli ($server, $user, $pass, $db);
+
+if($conexion->connect_errno) {
+  die("La conexión fallo" . $$conexion->connect_errno);
+}
