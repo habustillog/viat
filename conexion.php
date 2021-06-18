39e86371c16710a128161664c73a8e392f7e1abf
habustillog <79932434+habustillog@users.noreply.github.com>
2021-06-18 08:18:02 -0500

    update

--- conexion.php
+++ conexion.php
@@ -10,7 +10,7 @@ $db = "viats";
 $conexion = new mysqli ($server, $user, $pass, $db);
 
 if($conexion->connect_errno) {
-  die("La conexión fallo" . $$conexion->connect_errno);
+  die("La conexión fallo" . $conexion->connect_errno);
 }else{
   echo "te rifaste";
 }
--- registro/sh.php
+++ registro/sh.php
@@ -1,24 +1,30 @@
 <?php
-require 'conexion.php';
+include ("conexion.php");
 
-$dn = $_POST['dn'];;
-$hospedaje = $_POST['hospedaje'];
-$empresa = $_POST['empresa'];
-$ubicacionllegada = $_POST['dubicacionllegada'];
-$presupuesto = $_POST['presupuesto'];
-$diallegada = $_POST['diallegada'];
-$horallegada = $_POST['horadellegada'];
-$diasalida = $_POST['diasalida'];
-$horasalida = $_POST['horadesalida'];
 
-$insertar = "INSERT INTO solicitudhospedaje VALUES ('$dn','$hospedaje','$empresa','$presupuesto','$ubicacionllegada','$diallegada','$horadellegada','$diadesalida','$horadesalida')";
+$dn = $_POST["dn"];
+$hospedaje = $_POST["hospedaje"];
+$empresa = $_POST["empresa"];
+$ubicacion = $_POST["ubicacion"];
+$presupuesto = $_POST["presupuesto"];
+$diallegada = $_POST["diallegada"];
+$horallegada = $_POST["horallegada"];
+$diasalida = $_POST["diasalida"];
+$horasalida = $_POST["horasalida"];
 
-$query = mysqli_query($conexion, $insertar);
 
-if($query){
-  echo "bien puto";
+
+
+$insertar = "INSERT INTO solicitudhospedaje (DN, Hospedaje, Empresa, Presupuesto, Ubicación, Diallegada, Horallegada, DiaSalida, HoraSalida) VALUES ('$dn','$hospedaje','$empresa','$presupuesto','$ubicacion','$diallegada','$horallegada','$diasalida','$horasalida')";
+
+
+$resultado = mysqli_query ($conexion, $insertar);
+
+if($resultado){
+  echo "<script>alert('bien puto');window.location='/viat'</script>";
+
 }
 else {
-  echo "basura";
+  echo "<script>alert('bien puto');window.history.go(-1);</script>";
 }
  ?>
