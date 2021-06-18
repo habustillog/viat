7f4b3ded12ae81cc19f82b0866a7252f6a1b21a2
habustillog <79932434+habustillog@users.noreply.github.com>
2021-06-18 13:56:50 -0500

    up

--- registro/sh.php
+++ registro/sh.php
@@ -1,7 +1,6 @@
 <?php
-include ("conexion.php");
-
-
+include '../conexion.php';
+//Recibir los Datos
 $dn = $_POST["dn"];
 $hospedaje = $_POST["hospedaje"];
 $empresa = $_POST["empresa"];
@@ -14,17 +13,26 @@ $horasalida = $_POST["horasalida"];
 
 
 
+for ($i=0;$i<count($hospedaje);$i++)
+{
+  $hospedaje1 = $hospedaje[$i];
+echo "$dn $hospedaje1 $empresa $ubicacion $presupuesto $diallegada $horallegada $diasalida $horasalida";
+}
 
-$insertar = "INSERT INTO solicitudhospedaje (DN, Hospedaje, Empresa, Presupuesto, Ubicación, Diallegada, Horallegada, DiaSalida, HoraSalida) VALUES ('$dn','$hospedaje','$empresa','$presupuesto','$ubicacion','$diallegada','$horallegada','$diasalida','$horasalida')";
 
 
+//Consulta para insertar
+$insertar = "INSERT INTO solicitudhospedaje(DN, Hospedaje, Empresa, Presupuesto, Ubicacion, Diallegada, Horallegada, DiaSalida, HoraSalida) VALUES ('$dn','$hospedaje1','$empresa','$presupuesto','$ubicacion','$diallegada','$horallegada','$diasalida','$horasalida')";
+//Ejectuar cosnsulta
 $resultado = mysqli_query ($conexion, $insertar);
-
-if($resultado){ 
-  echo "<script>alert('bien puto');window.location='/viat'</script>";
+if(!$resultado){
+  echo 'Error' . mysqli_error($conexion);
 
 }
 else {
-  echo "<script>alert('bien puto');window.history.go(-1);</script>";
+  echo "<script>alert('Tu solicitud ha sido enviada, se te informara el proceso por correo electrónico, cualquier duda o aclaración favor de mandar un correo a habustillog@gptservices.com');
+window.location= '../index.php'
+  </script>";
 }
- ?>
+//Cerrar conexión
+mysqli_close($conexion);
