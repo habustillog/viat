1fccf18a79d472c7da187073fff5bcaa17d55033
habustillog <79932434+habustillog@users.noreply.github.com>
2021-06-21 10:25:57 -0500

    up

---
+++ registro/st.php
@@ -0,0 +1,37 @@
+<?php
+include '../conexion.php';
+//Recibir los Datos
+$dn = $_POST["dn"];
+$transporte = $_POST["transporte"];
+$empresa = $_POST["empresa"];
+$presupuesto = $_POST["presupuesto"];
+$diallegada = $_POST["diallegada"];
+$horallegada = $_POST["horallegada"];
+$diasalida = $_POST["diasalida"];
+$horasalida = $_POST["horasalida"];
+
+
+
+for ($i=0;$i<count($transporte);$i++)
+{
+  $transporte1 = $transporte[$i];
+echo "$dn $transporte1 $empresa $presupuesto $diallegada $horallegada $diasalida $horasalida";
+}
+
+
+
+//Consulta para insertar
+$insertar = "INSERT INTO solicitudtransporte(DN, Transporte, Empresa, Presupuesto, DiaLLegada, HoraLLegada, DiaSalida, HoraSalida) VALUES ('$dn','$transporte1','$empresa','$presupuesto','$diallegada','$horallegada','$diasalida','$horasalida')";
+//Ejectuar cosnsulta
+$resultado = mysqli_query ($conexion, $insertar);
+if(!$resultado){
+  echo 'Error' . mysqli_error($conexion);
+
+}
+else {
+  echo "<script>alert('Tu solicitud ha sido enviada, se te informara el proceso por correo electrónico, cualquier duda o aclaración favor de mandar un correo a habustillog@gptservices.com');
+window.location= '../index.php'
+  </script>";
+}
+//Cerrar conexión
+mysqli_close($conexion);
