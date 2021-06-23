0116b1c0aa6eb9aa75ac44e488e4ffd72e9b2775
habustillog <79932434+habustillog@users.noreply.github.com>
2021-06-23 14:04:32 -0500

    j

--- assets/header.php
+++ assets/header.php
@@ -20,8 +20,8 @@
   <body>
 
 <nav class="navbar navbar-light bg-light">
-  <a class="navbar-brand" href="index.php">
-    <img src="assets/img/logo.png" width="140" height="50" alt="">
+  <a class="navbar-brand" href="../index.php">
+    <img src="assets/img/logo.png" width="140" height="50" alt="Logo">
   </a>
     <a href="../index.php"><button class="btn btn-primary"type="button" name="button">REGRESAR</button></a>
   <a href="login1.php"><button class="btn btn-danger"type="button" name="button">CERRAR SESIÓN</button></a>
