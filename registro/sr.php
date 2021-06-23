05fa93f6ed668afdc40f7fc40e03390518399be0
habustillog <79932434+habustillog@users.noreply.github.com>
2021-06-23 14:04:40 -0500

    j

--- registro/sr.php
+++ registro/sr.php
@@ -1,31 +1,34 @@
 <?php
 include '../conexion.php';
 //Recibir los Datos
+// if (
+//   isset($_POST['dn']) && !empty($_POST['dn']) &&
+//   isset($_POST['dn']) && !empty($_POST['dn'])
+//   )
+
+
 $dn = $_POST["dn"];
 $motivo = $_POST["motivo"];
-$evidencia = $_POST["evidencia"];
+$cantidad = $_POST["cantidad"];
+$evidencia = $_POST['evidencia']['tmp_name'];
+echo "DN: $dn <br>";
+echo "motivo: $motivo<br>";
+echo "cantidad: $cantidad<br>";
+echo "evi: $evidencia<br>";
 
+if ( !empty($_FILES['evidencia']) && is_uploaded_file($_FILES['evidencia']['tmp_name'])) {
 
+  $imgData = addslashes(file_get_contents($_FILES['evidencia']['tmp_name']));
+  $imageProperties = getimageSize($_FILES['evidencia']['tmp_name']);
 
+  echo "{$imageProperties['mime']} {$imgData} ";
 
+    // $sql = "INSERT INTO solicitudreembolso(DN , Motivo, Evidencia, Canttidad) VALUES('$dn','$motivo', '{$imageProperties['mime']}', '{$imgData}', '$cantidad')";
+    // $current_id = mysqli_query($conn, $sql) or die("<b>Error:</b> Problem on Image Insert<br/>" . mysqli_error($conexion));
+    // if (isset($current_id)) {
+    //   header("Location: listImages.php");
+    // }
 
-
-
-//Consulta para insertar
-$insertar = "INSERT INTO solicitudreembolso (DN, Motivo, Evidencia) VALUES ('$dn','$motivo','$evidencia',)";
-//Ejectuar cosnsulta
-$resultado = mysqli_query ($conexion, $insertar);
-if(!$resultado){
-  echo 'Error' . mysqli_error($conexion);
-
+}else {
+  echo "string";
 }
-else {
-  echo "<script>alert('Tu solicitud ha sido enviada, se te informara el proceso por correo electrónico, cualquier duda o aclaración favor de mandar un correo a habustillog@gptservices.com');
-window.location= '../index.php'
-  </script>";
-}
-//Cerrar conexión
-mysqli_close($conexion);
-
-
- if (count($_evidencia) > 0) { if (is_uploaded_file($evidencia['userImage']['tmp_name'])) { require_once "db.php"; $imgData = addslashes(file_get_contents($_FILES['userImage']['tmp_name'])); $imageProperties = getimageSize($_FILES['userImage']['tmp_name']); $sql = "INSERT INTO output_images(imageType ,imageData) VALUES('{$imageProperties['mime']}', '{$imgData}')"; $current_id = mysqli_query($conn, $sql) or die("<b>Error:</b> Problem on Image Insert<br/>" . mysqli_error($conn)); if (isset($current_id)) { header("Location: listImages.php"); } } }
