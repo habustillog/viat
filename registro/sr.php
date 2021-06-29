c4fe6e5b4f661b12f37e850a13972acaa332858d
habustillog <79932434+habustillog@users.noreply.github.com>
2021-06-29 08:44:46 -0500

    up

--- registro/sr.php
+++ registro/sr.php
@@ -1,44 +1,38 @@
 <?php
 include '../conexion.php';
+//Validar Datos vacios
+if (
+  isset($_POST['dn']) && !empty($_POST['dn']) &&
+  isset($_POST['motivo']) && !empty($_POST['motivo']) &&
+  isset($_POST['cantidad']) && !empty($_POST['cantidad'])
+  )
+{
+
 //Recibir los Datos
-// if (
-//   isset($_POST['dn']) && !empty($_POST['dn']) &&
-//   isset($_POST['dn']) && !empty($_POST['dn'])
-//   )
+$dn = $_POST["dn"];
+$motivo = $_POST["motivo"];
+$cantidad = $_POST["cantidad"];
 
-
-// $dn = $_POST["dn"];
-// $motivo = $_POST["motivo"];
-// $cantidad = $_POST["cantidad"];
-
-echo "DN: $dn <br>";
-echo "motivo: $motivo<br>";
-echo "cantidad: $cantidad<br>";
-
-if(isset($_POST["submit"])){
-    $revisar = getimagesize($_FILES["evidencia"]["tmp_name"]);
-    if($revisar !== false)
-        $image = $_FILES['evidencia']['tmp_name'];
-        $imgContenido = addslashes(file_get_contents($image));
-
+if ( !empty($_FILES["evidencia"])) {
+  echo $_FILES["evidencia"]['tmp_name'];
+}else {
+  echo "string";
 }
-        echo "$imagContenido" ;
-else{
-  echo "vlv"
+    $insertar = "INSERT INTO solicitudreembolso(DN , Motivo, Cantidad, Evidencia) VALUES('$dn','$motivo', '$cantidad', '$evidencia')";
+     //Ejectuar cosnsulta
+     $resultado = mysqli_query ($conexion, $insertar);
+     if(!$resultado){
+       echo 'Error' . mysqli_error($conexion);
+
+     }
+     else {
+       echo "<script>alert('Tu solicitud ha sido enviada, se te informara el proceso por correo electrónico, cualquier duda o aclaración favor de mandar un correo a habustillog@gptservices.com');
+     window.location= '../index.php'
+       </script>";
+     }
+     //Cerrar conexión
+     mysqli_close($conexion);
 }
-// if ( !empty($_FILES['evidencia']) && is_uploaded_file($_FILES['evidencia']['tmp_name'])) {
-//
-//   $imgData = addslashes(file_get_contents($_FILES['evidencia']['tmp_name']));
-//   $imageProperties = getimageSize($_FILES['evidencia']['tmp_name']);
-
-  // echo "{$imageProperties['mime']} {$imgData} ";
-
-    // $sql = "INSERT INTO solicitudreembolso(DN , Motivo, Evidencia, Canttidad) VALUES('$dn','$motivo', '{$imageProperties['mime']}', '{$imgData}', '$cantidad')";
-    // $current_id = mysqli_query($conn, $sql) or die("<b>Error:</b> Problem on Image Insert<br/>" . mysqli_error($conexion));
-    // if (isset($current_id)) {
-    //   header("Location: listImages.php");
-    // }
-
-// }else {
-//   echo "string";
-// }
+else {
+  echo "Error";
+}
