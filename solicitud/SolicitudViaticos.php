2a87ceb752069481efa64bebd05c625fc463fa27
habustillog <79932434+habustillog@users.noreply.github.com>
2021-06-30 15:06:29 -0500

    a

--- solicitud/SolicitudViaticos.php
+++ solicitud/SolicitudViaticos.php
@@ -18,7 +18,7 @@
 <body>
   <header>
     <?php
-       require_once('header.php')
+       require_once ('header.php')
      ?>
   </header>
   <main>
@@ -61,7 +61,7 @@
 
 <footer>
 <?php
-require_once('../assets/footer.php')
+require_once ('../assets/footer.php')
  ?>
   </footer>
 
