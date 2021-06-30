a2a6148e1637918abf0651fa2a9bf0782266ec8f
habustillog <79932434+habustillog@users.noreply.github.com>
2021-06-30 15:06:21 -0500

    d

--- solicitud/SolicitudTransporte.php
+++ solicitud/SolicitudTransporte.php
@@ -21,7 +21,7 @@
    <body>
      <header>
        <?php
-         require_once('header.php')
+         require_once ('header.php')
         ?>
      </header>
      <main>
@@ -90,7 +90,7 @@
 </main>
      <footer>
    <?php
-   require_once('../assets/footer.php')
+   require_once ('../assets/footer.php')
     ?>
      </footer>
    </body>
