c820fcbe4573190fa12800615d2f7278b49ad7f2
habustillog <79932434+habustillog@users.noreply.github.com>
2021-06-30 08:52:50 -0500

    d

--- login.php
+++ login.php
@@ -1,49 +1,99 @@
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<meta name="google-signin-client_id" content="647327890949-p258favcg5medm53tegot4li91nuuvus.apps.googleusercontent.com">
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <title>Loginini</title>
-</head>
-<body>
-
-    <div id="logreg-forms">
-        <form class="form-signin" style="background: rgb(255, 255, 255);">
-          <div class="h3 mb-3 font-weight-normal" style="text-align: center">
-            <img src="assets/img/logo.png" alt="Gpt Services">
-          </div>
-            <h1 class="h3 mb-3 font-weight-normal" style="text-align: center; font-size:20;">Utiliza la dirección de correo electrónico que se otorgo en el trabajo.</h1>
-            <div class="social-login h3 mb-3 font-weight-normal" style="text-align: center">
-
-              <a href="index.php"><button class="btn google-btn social-btn" onclick="index.php" type="button" style="width:350px;" ><span style="font-size:20" ><i class="fab fa-google-plus-g" ></i>Conectarse con Google</span> </button></a>
-            </div>
-
-</form>
-
-    </div>
-  <footer>
 <?php
-require_once('assets/footer.php')
- ?>
-  </footer>
+
+//index.php
+
+//Include Configuration File
+include('config.php');
+
+$login_button = '';
 
 
-</body>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script src="assets/js/script.js"></script>
-<script src="https://apis.google.com/js/platform.js" async defer></script>
+if(isset($_GET["code"]))
+{
+
+ $token = $google_client->fetchAccessTokenWithAuthCode($_GET["code"]);
+ if(!isset($token['error']))
+ {
+
+  $google_client->setAccessToken($token['access_token']);
+
+
+  $_SESSION['access_token'] = $token['access_token'];
+
+
+  $google_service = new Google_Service_Oauth2($google_client);
+
+
+  $data = $google_service->userinfo->get();
+
+
+  if(!empty($data['given_name']))
+  {
+   $_SESSION['user_first_name'] = $data['given_name'];
+  }
+
+  if(!empty($data['family_name']))
+  {
+   $_SESSION['user_last_name'] = $data['family_name'];
+  }
+
+  if(!empty($data['email']))
+  {
+   $_SESSION['user_email_address'] = $data['email'];
+  }
+
+  if(!empty($data['gender']))
+  {
+   $_SESSION['user_gender'] = $data['gender'];
+  }
+
+  if(!empty($data['picture']))
+  {
+   $_SESSION['user_image'] = $data['picture'];
+  }
+ }
+}
+
+
+if(!isset($_SESSION['access_token']))
+{
+ $login_button = '<a href="'.$google_client->createAuthUrl().'">Login With Google</a>';
+}
+
+?>
+<html>
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>PHP Login using Google Account</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+ </head>
+ <body>
+  <div class="container">
+   <br />
+   <h2 align="center">PHP Login using Google Account</h2>
+   <br />
+   <div class="panel panel-default">
+   <?php
+   if($login_button == '')
+   {
+    echo '<div class="panel-heading">Welcome User</div><div class="panel-body">';
+    echo '<img src="'.$_SESSION["user_image"].'" class="img-responsive img-circle img-thumbnail" />';
+    echo '<h3><b>Name :</b> '.$_SESSION['user_first_name'].' '.$_SESSION['user_last_name'].'</h3>';
+    echo '<h3><b>Email :</b> '.$_SESSION['user_email_address'].'</h3>';
+    echo '<h3><a href="logout.php">Logout</h3></div>';
+   }
+   else
+   {
+    echo '<div align="center">'.$login_button . '</div>';
+   }
+   ?>
+   </div>
+  </div>
+ </body>
 </html>
