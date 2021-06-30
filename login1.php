1d25ecf938f1a8814be178ccea1799bf0826aa43
habustillog <79932434+habustillog@users.noreply.github.com>
2021-06-30 08:52:57 -0500

    d

--- login1.php
+++ login1.php
@@ -1,3 +1,67 @@
+<?php
+
+//index.php
+
+//Include Configuration File
+include('config.php');
+
+$login_button = '';
+
+
+if(isset($_GET["code"]))
+{
+
+ $token = $google_client->fetchAccessTokenWithAuthCode($_GET["code"]);
+ if(!isset($token['error']))
+ {
+
+  $google_client->setAccessToken($token['access_token']);
+
+
+  $_SESSION['access_token'] = $token['access_token'];
+
+
+  $google_service = new Google_Service_Oauth2($google_client);
+
+
+  $data = $google_service->userinfo->get();
+
+
+  if(!empty($data['given_name']))
+  {
+   $_SESSION['user_first_name'] = $data['given_name'];
+  }
+
+  if(!empty($data['family_name']))
+  {
+   $_SESSION['user_last_name'] = $data['family_name'];
+  }
+
+  if(!empty($data['email']))
+  {
+   $_SESSION['user_email_address'] = $data['email'];
+  }
+
+  if(!empty($data['gender']))
+  {
+   $_SESSION['user_gender'] = $data['gender'];
+  }
+
+  if(!empty($data['picture']))
+  {
+   $_SESSION['user_image'] = $data['picture'];
+  }
+ }
+}
+
+
+if(!isset($_SESSION['access_token']))
+{
+ $login_button = '<a href="'.$google_client->createAuthUrl().'">Login With Google</a>';
+}
+
+?>
+
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -16,22 +80,19 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
-    <title>Login</title>
+    <title>Loginini</title>
 </head>
 <body>
 
     <div id="logreg-forms">
-        <form class="form-signin" style="background: rgb(255, 255, 255);"  action="validar.php" method="post">
+        <form class="form-signin" style="background: rgb(255, 255, 255);">
           <div class="h3 mb-3 font-weight-normal" style="text-align: center">
             <img src="assets/img/logo.png" alt="Gpt Services">
           </div>
-          <label for="email" class="control-label">Correo de Gpt Services</label>
-          <input id="email" name="email" type="email" placeholder="ejemplo@gptservices.com" class="form-control">
-          <label for="password" class="control-label">Contraseña</label>
-          <input id="password" name="password" type="password" placeholder="Contraseña" class="form-control">
+            <h1 class="h3 mb-3 font-weight-normal" style="text-align: center; font-size:20;">Utiliza la dirección de correo electrónico que se otorgo en el trabajo.</h1>
             <div class="social-login h3 mb-3 font-weight-normal" style="text-align: center">
 
-            <button class="btn google-btn social-btn" type="submit" style="width:350px;" ><span style="font-size:20" >Iniciar sesión</span> </button>
+              <a href="index.php"><button class="btn google-btn social-btn" onclick="index.php" type="button" style="width:350px;" ><span style="font-size:20" ><i class="fab fa-google-plus-g" ></i>Conectarse con Google</span> </button></a>
             </div>
 
 </form>
